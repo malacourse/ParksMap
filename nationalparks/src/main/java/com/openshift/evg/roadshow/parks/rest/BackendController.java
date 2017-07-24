e3a265bdaf29594426b13f9b4981fa14d918cbc7
malacourse <malacourse>
2017-07-24 13:40:12 -0400

    Chnages

--- nationalparks/src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ nationalparks/src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -2,6 +2,8 @@ package com.openshift.evg.roadshow.parks.rest;
 
 import com.openshift.evg.roadshow.rest.gateway.model.Backend;
 import com.openshift.evg.roadshow.rest.gateway.model.Coordinates;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * Created by jmorales on 26/09/16.
  */
-@RequestMapping("/ws/info")
+@RequestMapping("/ws")
 @RestController
 public class BackendController{
 
-    @RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET, value = "/info", produces = "application/json")
     public Backend get() {
         return new Backend("nationalparks","National Parks", new Coordinates("47.039304", "14.505178"), 4);
     }
